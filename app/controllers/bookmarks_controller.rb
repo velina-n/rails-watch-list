099465ec6f7cb37bb6004d6a0d6a3575fe01f3c9
velina negovanska <vnegovanska@gmail.com>
2024-11-15 12:33:06 +0100

    test js

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    raise
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+end
---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list, message: "is already in the list" }
+end
