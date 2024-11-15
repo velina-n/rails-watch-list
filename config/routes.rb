de84e07e270c79029184adbe14c5397e01a727ad
velina negovanska <vnegovanska@gmail.com>
2024-11-15 16:38:02 +0100

    Corrections

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :lists do
+  resources :lists, except: :index do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
